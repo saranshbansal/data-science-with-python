89b19a9d4bdf7515eb8d5ffee82118d5e0f620a7
Saransh Bansal <sbansal@shootsta.com>
2018-06-07 00:38:04 +0530

    improved description of case study.

--- com.saransh.py.scripts/case_study_urban_population_trends.py
+++ com.saransh.py.scripts/case_study_urban_population_trends.py
@@ -1,25 +1,22 @@
-# Writing an iterator to load data in chunks (5)
+# Case Study: Plot Urban population trends in various countries over the years based on publically available data set.
 #
-# This is the last leg. You've learned a lot about processing a large
-# dataset in chunks. In this last exercise, you will put all the code for processing the data into a single function
-# so that you can reuse the code without having to rewrite the same things all over again.
+# In this case study, I have to define the function plot_pop() which takes two arguments: the filename of the file to
+# be processed, and the country code of the rows we want to process in the dataset.
 #
-# You're going to define the function plot_pop() which takes two arguments: the filename of the file to be processed,
-#  and the country code of the rows you want to process in the dataset.
+# calling the function already does the following:
 #
-# Because all of the previous code you've written in the previous exercises will be housed in plot_pop(), calling the
-#  function already does the following:
+# Loading of the file chunk by chunk,
+# Creating the new column of urban population values,
+# and Plotting the urban population data.
 #
-# Loading of the file chunk by chunk, Creating the new column of urban population values, and Plotting the urban
-# population data. That's a lot of work, but the function now makes it convenient to repeat the same process for
-# whatever file and country code you want to process and visualize!
+# The function makes it convenient to repeat the same process for whatever file and country code we want to process
+# and visualize!
 #
-# You're going to use the data from 'ind_pop_data.csv', available in your current directory. The packages pandas and
-# matplotlib.pyplot has been imported as pd and plt respectively for your use.
+# We are using the data from 'ind_pop_data.csv', available in /resources/ directory.
+# The packages pandas and matplotlib.pyplot has been imported as pd and plt respectively.
 #
-# After you are done, take a moment to look at the plots and reflect on the new skills you have acquired. The journey
-#  doesn't end here! If you have enjoyed working with this data, you can continue exploring it using the
-# pre-processed version available on Kaggle.
+# If you have enjoyed working with this data, you can continue exploring it using the pre-processed version available
+# on Kaggle.
 
 import pandas as pd
 import matplotlib.pyplot as plt
