71893ef9f5c1e5caa264af4a3736282cf4d0c9ac
Saransh Bansal <saran3h.bansal@gmail.com>
2018-06-24 02:11:42 +0530

    hdf & matlab file reading added.

---
+++ com.saransh.py.scripts/py_read_hdf5_file.py
@@ -0,0 +1,16 @@
+import h5py
+
+path = '../resources/'
+
+# Assign filename: file
+file = 'NEONDS.hdf5'
+
+# Load file: data
+data = h5py.File(path + file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(data[key])
---
+++ com.saransh.py.scripts/py_read_matlab_file.py
@@ -0,0 +1,30 @@
+# Import package
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = '../resources/'
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat(path + 'albeck_gene_expression.mat')
+
+# Print the datatype type of mat
+print(type(mat))
+
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['fret']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(np.shape(mat['fret']))
+
+# Subset the array and plot it
+data = mat['fret'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
+
