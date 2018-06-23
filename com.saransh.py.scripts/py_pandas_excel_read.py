4ac2d3e713bcc76388686b9f350932fccddf256b
Saransh Bansal <saran3h.bansal@gmail.com>
2018-06-23 13:58:42 +0530

    few more datasets

--- com.saransh.py.scripts/py_pandas_excel_read.py
+++ com.saransh.py.scripts/py_pandas_excel_read.py
@@ -16,11 +16,28 @@
 # Import pandas
 import pandas as pd
 
+path = '../resources/'
+
 # Assign spreadsheet filename: file
-file = 'battledeath.xlsx'
+file = 'battledeath_3.1.xlsx'
 
 # Load spreadsheet: xl
-xl = pd.ExcelFile(file)
+xl = pd.ExcelFile(path + file)
 
 # Print sheet names
 print(xl.sheet_names)
+
+# -------------------------------------------
+# Load a sheet into a DataFrame by name: df1
+df1 = xl.parse('bdonly')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xl.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+# PS: both are same!
