fe09ec6f888c79a8cd1c8225f75d0c96082a78f4
Saransh Bansal <saran3h.bansal@gmail.com>
2019-04-26 18:48:58 +0530

    no message

--- src/db/py_sql_with_pandas.py
+++ src/db/py_sql_with_pandas.py
@@ -1,4 +1,7 @@
 # Import necessary module
+import random
+from datetime import datetime
+
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -19,8 +22,19 @@ df4 = pd.read_sql_query(
     'select * from PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId where Milliseconds < 250000',
     engine)
 
+rand_dates = [datetime(random.randrange(2000, 2001), random.randrange(1, 6), random.randrange(1, 3)) for d in
+              range(0, len(df4))]
+df4['dates'] = rand_dates
+
+df4 = df4.loc[:, ~df4.columns.duplicated()]
 # Print head of DataFrame
-print(df1.head())
-print(df2.head())
-print(df3.head())
-print(df4.head())
+# print(df1.head())
+# print(df2.head())
+# print(df3.head())
+# print(df4.head())
+df4 = df4.groupby([df4.dates.dt.year.rename('year'), df4.dates.dt.month.rename('month')]).size()
+print(df4)
+# df4.groupby(df4.dates).agg({'count'})
+# print(df4.columns)
+# print(df4['dates'].value_counts())
+
