3ab4ef22fa0841b760bb15432a31607866167251
Saransh Bansal <saran3h.bansal@gmail.com>
2019-06-10 09:20:19 +0530

    new package for interview questions

--- src/core/py_regex.py
+++ src/core/py_regex.py
@@ -1,9 +1,24 @@
 import re
+from datetime import datetime
 
-str = '101010'
-pattern = '0'
-repl_str = '2'
+mytext = str([
+    '20080620033027/http://www.mrvc.indianrail.gov.in/overview.htm). _Official webpage of Mumbai Railway Vikas Corporation_. Archived from [the original](http://www.mrvc.indianrail.gov.in/overview.htm) on 2008-06-20. Retrieved 2008-12-11.'])
 
-result = re.sub(pattern, repl_str, str)
+myregex = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
+x = re.findall(myregex, mytext)
 
-print(result)
+result = []
+for res in x:
+    result.append(res.replace("www.", "").split('//')[-1].split('/')[0])
+
+print(';'.join(result))
+
+a = [x for x in range(3, 13)]
+print(a)
+value = []
+print(value[0] if value else 0)
+
+val = '2018,1'
+date_object = datetime.strptime(val, '%Y,%m')
+
+print(date_object.strftime("%b %y"))
---
+++ src/python_core/output_questions/1.py
@@ -0,0 +1,9 @@
+# what will be the output?
+
+n = [1, 2, 5, 10, 3, 100, 9, 24]
+
+for e in n:
+    if e < 5:
+        n.remove(e)
+
+print(n)
